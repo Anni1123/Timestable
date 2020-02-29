b458c4f527ceef38051156e4584543cce0587cac
Anni1123 <kumarianni1213@gmail.com>
2020-02-29 14:39:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/timestable/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.timestable;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView timesTableListView;
+
+    public void generateTimesTable(int timesTable) {
+
+        ArrayList<String> timesTableContent = new ArrayList<String>();
+
+        for (int i = 1; i <= 10; i++) {
+
+            timesTableContent.add(Integer.toString(i * timesTable));
+
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, timesTableContent);
+
+        timesTableListView.setAdapter(arrayAdapter);
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final SeekBar timesTableSeekBar = (SeekBar)findViewById(R.id.timesTableSeekBar);
+        timesTableListView = (ListView)findViewById(R.id.timesTableListView);
+
+        timesTableSeekBar.setMax(20);
+        timesTableSeekBar.setProgress(10);
+
+        timesTableSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                int min = 1;
+                int timesTable;
+
+                if (progress < min) {
+
+                    timesTable = min;
+                    timesTableSeekBar.setProgress(min);
+
+                } else {
+
+                    timesTable = progress;
+
+                }
+
+                generateTimesTable(timesTable);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        generateTimesTable(10);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
